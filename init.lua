55a43beb9d08cb8fc5283508af2efe61dc732021
Traxie21 <traxie21@gmail.com>
2013-04-16 15:41:33 -0500

    Added Thrown potion particle, so that you can see the flying potion.

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+local MOD_NAME = minetest.get_current_modname()
+   local MOD_PATH = minetest.get_modpath(MOD_NAME)
+   local Vec3 = dofile(MOD_PATH.."/lib/Vec3_1-0.lua")
+
 potions = {}
 
 
@@ -15,22 +19,26 @@ function potions.register_potion(iname, color, exptime, action, expaction)
 			--Particle Code
 			--Potions Particles
 			minetest.add_particlespawner(30, 0.2,
-   		 pointed_thing.above, pointed_thing.above,
-   		 {x=1, y= 2, z=1}, {x=-1, y= 2, z=-1},
-   		 {x=0.2, y=0.2, z=0.2}, {x=-0.2, y=0.5, z=-0.2},
-   		 5, 10,
-   		 1, 3,
-  			 false, "potions_"..color..".png")
+				pointed_thing.above, pointed_thing.above,
+				{x=1, y= 2, z=1}, {x=-1, y= 2, z=-1},
+				{x=0.2, y=0.2, z=0.2}, {x=-0.2, y=0.5, z=-0.2},
+				5, 10,
+				1, 3,
+				false, "potions_"..color..".png")
   			 
   			 --Shatter Particles
   			 minetest.add_particlespawner(40, 0.1,
-   		 pointed_thing.above, pointed_thing.above,
-   		 {x=2, y=0.2, z=2}, {x=-2, y=0.5, z=-2},
-   		 {x=0, y=-6, z=0}, {x=0, y=-10, z=0},
-   		 0.5, 2,
-   		 0.2, 5,
-  			 true, "potions_shatter.png")
-  			 
+				pointed_thing.above, pointed_thing.above,
+				{x=2, y=0.2, z=2}, {x=-2, y=0.5, z=-2},
+				{x=0, y=-6, z=0}, {x=0, y=-10, z=0},
+				0.5, 2,
+				0.2, 5,
+				true, "potions_shatter.png")
+				
+				local dir = Vec3(user:get_look_dir()) *20
+				minetest.add_particle(
+				{x=user:getpos().x, y=user:getpos().y+1.5, z=user:getpos().z}, {x=dir.x, y=dir.y, z=dir.z}, {x=0, y=-10, z=0}, 0.2,
+					6, false, "potions_bottle.png^potions_"..color..".png")
 			return itemstack
 			
 		end,

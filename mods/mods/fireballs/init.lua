595e67cb4c333bf639d0c46dc8f2e2d19657205f
Chris Routh <routh@amateurwriting.net>
2015-07-18 15:07:48 -0400

    Removing duplicated mods folder

--- mods/mods/fireballs/init.lua
+++
@@ -1,8 +0,0 @@
-modpath=minetest.get_modpath("fireballs")
-
-dofile(modpath.."/fireball.lua")
-dofile(modpath.."/light_ice.lua")
-dofile(modpath.."/thunder_sword.lua")
-dofile(modpath.."/smokeball.lua")
-dofile(modpath.."/earthball.lua")
-dofile(modpath.."/ultimate_weapon.lua")

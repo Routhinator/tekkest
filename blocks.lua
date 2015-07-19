e3432c210e2f5ff7381c6f1069dd79d056393423
Chris Routh <routh@amateurwriting.net>
2015-07-19 18:18:32 -0400

    Adding Tar block.

---
+++ blocks.lua
@@ -0,0 +1,8 @@
+minetest.register_node("moreliquids:tar_block", {
+  description = "Tar Block",
+	tiles = {"tar.png"},
+	groups = {fleshy=3, falling_node=1},
+	sounds = default.node_sound_tar_defaults({
+		footstep = {name="default_tar_footstep", gain=0.25},
+	}),
+})
---
+++ init.lua
@@ -0,0 +1,2 @@
+dofile(minetest.get_modpath(minetest.get_current_modname()).."/blocks.lua")
+dofile(minetest.get_modpath(minetest.get_current_modname()).."/crafts.lua")

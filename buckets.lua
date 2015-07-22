e7ba716410c74cdc48d0590074f9b29ee77f37e7
Chris Routh <routh@amateurwriting.net>
2015-07-21 21:14:18 -0400

    Updating code changes.

--- buckets.lua
+++ buckets.lua
@@ -2,33 +2,38 @@ bucket.register_liquid(
 	"moreliquids:oil_source",
 	"moreliquids:oil_flowing",
 	"moreliquids:bucket_oil",
-	"oil_bucket.png"
+	"oil_bucket.png",
+  "Oil Bucket"
 )
 
 bucket.register_liquid(
 	"moreliquids:fuel_source",
 	"moreliquids:fuel_flowing",
 	"moreliquids:bucket_fuel",
-	"fuel_bucket.png"
+	"fuel_bucket.png",
+  "Fuel Bucket"
 )
 
 bucket.register_liquid(
 	"moreliquids:sewage_source",
 	"moreliquids:sewage_flowing",
 	"moreliquids:bucket_sewage",
-	"sewage_bucket.png"
+	"sewage_bucket.png",
+  "Sewage Bucket"
 )
 
 bucket.register_liquid(
 	"moreliquids:toxic_waste_source",
 	"moreliquids:toxic_waste_flowing",
 	"moreliquids:bucket_toxic_waste",
-	"toxic_waste_bucket.png"
+	"toxic_waste_bucket.png",
+  "Toxic Waste Bucket"
 )
 
 bucket.register_liquid(
 	"moreliquids:cryogel_source",
 	"moreliquids:cryogel_flowing",
 	"moreliquids:bucket_cryogel",
-	"cryogel_bucket.png"
+	"cryogel_bucket.png",
+  "Cryogel Bucket"
 )

f18d06785050168ebb258174039f62eda8972235
Chris Routh <routh@amateurwriting.net>
2015-07-26 09:29:35 -0400

    Add intllib support function.

--- init.lua
+++ init.lua
@@ -1,3 +1,12 @@
+-- Boilerplate to support localized strings if intllib mod is installed.
+local S
+if (minetest.get_modpath("intllib")) then
+  dofile(minetest.get_modpath("intllib").."/intllib.lua")
+  S = intllib.Getter(minetest.get_current_modname())
+else
+  S = function ( s ) return s end
+end
+
 dofile(minetest.get_modpath(minetest.get_current_modname()).."/liquids.lua")
 dofile(minetest.get_modpath(minetest.get_current_modname()).."/buckets.lua")
 dofile(minetest.get_modpath(minetest.get_current_modname()).."/crafts.lua")

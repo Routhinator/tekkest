5b00780eb71aef1735bd1b3ae917a63e82a1503f
Chris Routh <routh@amateurwriting.net>
2015-07-26 08:53:48 -0400

    Refactor of all fuels to match more efficient code in technic.

--- buckets.lua
+++ buckets.lua
@@ -1,39 +1,27 @@
-bucket.register_liquid(
-	"moreliquids:oil_source",
-	"moreliquids:oil_flowing",
-	"moreliquids:bucket_oil",
-	"oil_bucket.png",
-  "Oil Bucket"
-)
+if bucket and bucket.register_liquid then
+    bucket.register_liquid("moreliquids:oil_source",
+        "moreliquids:oil_flowing",
+        "moreliquids:bucket_oil",
+        "oil_bucket.png",
+        "Oil Bucket")
 
-bucket.register_liquid(
-	"moreliquids:fuel_source",
-	"moreliquids:fuel_flowing",
-	"moreliquids:bucket_fuel",
-	"fuel_bucket.png",
-  "Fuel Bucket"
-)
+    bucket.register_liquid("moreliquids:fuel_source",
+        "moreliquids:fuel_flowing",
+        "moreliquids:bucket_fuel",
+        "fuel_bucket.png",
+        "Fuel Bucket")
 
-bucket.register_liquid(
-	"moreliquids:sewage_source",
-	"moreliquids:sewage_flowing",
-	"moreliquids:bucket_sewage",
-	"sewage_bucket.png",
-  "Sewage Bucket"
-)
+    bucket.register_liquid("moreliquids:sewage_source",
+        "moreliquids:sewage_flowing",
+        "moreliquids:bucket_sewage",
+        "sewage_bucket.png",
+        "Sewage Bucket")
 
-bucket.register_liquid(
-	"moreliquids:toxic_waste_source",
-	"moreliquids:toxic_waste_flowing",
-	"moreliquids:bucket_toxic_waste",
-	"toxic_waste_bucket.png",
-  "Toxic Waste Bucket"
-)
+    bucket.register_liquid("moreliquids:cryogel_source",
+        "moreliquids:cryogel_flowing",
+        "moreliquids:bucket_cryogel",
+        "cryogel_bucket.png",
+        "Cryogel Bucket")
+end
 
-bucket.register_liquid(
-	"moreliquids:cryogel_source",
-	"moreliquids:cryogel_flowing",
-	"moreliquids:bucket_cryogel",
-	"cryogel_bucket.png",
-  "Cryogel Bucket"
-)
+
--- liquids.lua
+++ liquids.lua
@@ -1,230 +1,156 @@
-OIL_VISC = 16
-OIL_ALPHA = 210
-FUEL_VISC = 1
-FUEL_ALPHA = 90
-SEWAGE_VISC = 7
-SEWAGE_ALPHA = 150
-TOXIC_WASTE_VISC = 6
-TOXIC_WASTE_ALPHA = 210
-CRYOGEL_VISC = 100
-CRYOGEL_ALPHA = 120
+for _, state in ipairs({ "flowing", "source" }) do
+    -- Cryogel
 
+    minetest.register_node("moreliquids:cryogel_" .. state, {
+        description = S(state == "source" and "Cryogel Source" or "Flowing Cryogel"),
+        drawtype = (state == "source" and "liquid" or "flowingliquid"),
+        [state == "source" and "tiles" or "special_tiles"] = {
+            {
+                name = "cryogel_" .. state .. "_animated.png",
+                animation = {
+                    type = "vertical_frames",
+                    aspect_w = 16,
+                    aspect_h = 16,
+                    length = 3.0,
+                },
+            }
+        },
+        paramtype = "light",
+        paramtype2 = (state == "flowing" and "flowingliquid" or nil),
+        light_source = (state == "source" and 8 or 5),
+        walkable = false,
+        pointable = false,
+        diggable = false,
+        buildable_to = true,
+        drop = "",
+        drowning = 1,
+        liquidtype = state,
+        liquid_alternative_flowing = "moreliquids:cryogel_flowing",
+        liquid_alternative_source = "moreliquids:cryogel_source",
+        liquid_viscosity = LAVA_VISC,
+        liquid_renewable = false,
+        damage_per_second = 6,
+        post_effect_color = { a = 192, r = 100, g = 203, b = 203 },
+        groups = {
+            liquid = 2,
+            cold = 6,
+            freezing = (state == "source" and 32000 or 16000),
+            not_in_creative_inventory = (state == "flowing" and 1 or nil),
+        },
+    })
 
--- Oil
+    -- Oil
 
-minetest.register_node("moreliquids:oil_flowing", {
-	description = "Oil (flowing)",
-	inventory_image = minetest.inventorycube("oil.png"),
-	drawtype = "flowingliquid",
-	tile_images = {"oil.png"},
-	paramtype = "light",
-	walkable = false,
-	pointable = false,
-	diggable = false,
-	buildable_to = true,
-	liquidtype = "flowing",
-	liquid_alternative_flowing = "moreliquids:oil_flowing",
-	liquid_alternative_source = "moreliquids:oil_source",
-	liquid_viscosity = OIL_VISC,
-	post_effect_color = {a=OIL_ALPHA, r=0, g=0, b=0},
-	special_materials = {
-		{image="oil.png", backface_culling=false},
-		{image="oil.png", backface_culling=true},
-	},
-})
+    minetest.register_node("moreliquids:oil_" .. state, {
+        description = S(state == "source" and "Oil Source" or "Flowing Oil"),
+        drawtype = (state == "source" and "liquid" or "flowingliquid"),
+        [state == "source" and "tiles" or "special_tiles"] = {
+            {
+                name = "oil_" .. state .. "_animated.png",
+                animation = {
+                    type = "vertical_frames",
+                    aspect_w = 16,
+                    aspect_h = 16,
+                    length = 3.0,
+                },
+            }
+        },
+        paramtype = "light",
+        paramtype2 = (state == "flowing" and "flowingliquid" or nil),
+        light_source = (state == "source" and 8 or 5),
+        walkable = false,
+        pointable = false,
+        diggable = false,
+        buildable_to = true,
+        drop = "",
+        drowning = 1,
+        liquidtype = state,
+        liquid_alternative_flowing = "moreliquids:oil_flowing",
+        liquid_alternative_source = "moreliquids:oil_source",
+        liquid_viscosity = LAVA_VISC,
+        liquid_renewable = false,
+        post_effect_color = { a = 210, r = 0, g = 0, b = 0 },
+        groups = {
+            liquid = 2,
+            flammable = 1,
+            not_in_creative_inventory = (state == "flowing" and 1 or nil),
+        },
+    })
 
-minetest.register_node("moreliquids:oil_source", {
-	description = "Oil",
-	inventory_image = minetest.inventorycube("oil.png"),
-	drawtype = "liquid",
-	tile_images = {"oil.png"},
-	paramtype = "light",
-	walkable = false,
-	pointable = false,
-	diggable = false,
-	buildable_to = true,
-	liquidtype = "source",
-	liquid_alternative_flowing = "moreliquids:oil_flowing",
-	liquid_alternative_source = "moreliquids:oil_source",
-	liquid_viscosity = OIL_VISC,
-	post_effect_color = {a=OIL_ALPHA, r=0, g=0, b=0},
-	special_materials = {
-		{image="oil.png", backface_culling=false},
-	},
-})
+    -- Fuel
 
--- Fuel
+    minetest.register_node("moreliquids:fuel_" .. state, {
+        description = S(state == "source" and "Fuel Source" or "Flowing Fuel"),
+        drawtype = (state == "source" and "liquid" or "flowingliquid"),
+        [state == "source" and "tiles" or "special_tiles"] = {
+            {
+                name = "fuel_" .. state .. "_animated.png",
+                animation = {
+                    type = "vertical_frames",
+                    aspect_w = 16,
+                    aspect_h = 16,
+                    length = 3.0,
+                },
+            }
+        },
+        paramtype = "light",
+        paramtype2 = (state == "flowing" and "flowingliquid" or nil),
+        light_source = (state == "source" and 8 or 5),
+        walkable = false,
+        pointable = false,
+        diggable = false,
+        buildable_to = true,
+        drop = "",
+        drowning = 1,
+        liquidtype = state,
+        liquid_alternative_flowing = "moreliquids:fuel_flowing",
+        liquid_alternative_source = "moreliquids:fuel_source",
+        liquid_viscosity = LAVA_VISC,
+        liquid_renewable = false,
+        post_effect_color = { a = 90, r = 233, g = 187, b = 3 },
+        groups = {
+            liquid = 2,
+            flammable = 1,
+            not_in_creative_inventory = (state == "flowing" and 1 or nil),
+        },
+    })
 
-minetest.register_node("moreliquids:fuel_flowing", {
-	description = "Fuel (flowing)",
-	inventory_image = minetest.inventorycube("fuel.png"),
-	drawtype = "flowingliquid",
-	tile_images = {"fuel.png"},
-	paramtype = "light",
-	walkable = false,
-	pointable = false,
-	diggable = false,
-	buildable_to = true,
-	liquidtype = "flowing",
-	liquid_alternative_flowing = "moreliquids:fuel_flowing",
-	liquid_alternative_source = "moreliquids:fuel_source",
-	liquid_viscosity = FUEL_VISC,
-	post_effect_color = {a=FUEL_ALPHA, r=233, g=187, b=3},
-	special_materials = {
-		{image="fuel.png", backface_culling=false},
-		{image="fuel.png", backface_culling=true},
-	},
-})
+    -- Sewage
 
-minetest.register_node("moreliquids:fuel_source", {
-	description = "Fuel",
-	inventory_image = minetest.inventorycube("fuel.png"),
-	drawtype = "liquid",
-	tile_images = {"fuel.png"},
-	paramtype = "light",
-	walkable = false,
-	pointable = false,
-	diggable = false,
-	buildable_to = true,
-	liquidtype = "source",
-	liquid_alternative_flowing = "moreliquids:fuel_flowing",
-	liquid_alternative_source = "moreliquids:fuel_source",
-	liquid_viscosity = FUEL_VISC,
-	post_effect_color = {a=FUEL_ALPHA, r=233, g=187, b=3},
-	special_materials = {
-		{image="fuel.png", backface_culling=false},
-	},
-})
+    minetest.register_node("moreliquids:sewage_" .. state, {
+        description = S(state == "source" and "Sewage Source" or "Flowing Sewage"),
+        drawtype = (state == "source" and "liquid" or "flowingliquid"),
+        [state == "source" and "tiles" or "special_tiles"] = {
+            {
+                name = "sewage_" .. state .. "_animated.png",
+                animation = {
+                    type = "vertical_frames",
+                    aspect_w = 16,
+                    aspect_h = 16,
+                    length = 3.0,
+                },
+            }
+        },
+        paramtype = "light",
+        paramtype2 = (state == "flowing" and "flowingliquid" or nil),
+        light_source = (state == "source" and 8 or 5),
+        walkable = false,
+        pointable = false,
+        diggable = false,
+        buildable_to = true,
+        drop = "",
+        drowning = 1,
+        liquidtype = state,
+        liquid_alternative_flowing = "moreliquids:sewage_flowing",
+        liquid_alternative_source = "moreliquids:sewage_source",
+        liquid_viscosity = WATER_VISC,
+        liquid_renewable = false,
+        post_effect_color = { a = 150, r = 172, g = 97, b = 0 },
+        groups = {
+            liquid = 2,
+            flammable = 1,
+            not_in_creative_inventory = (state == "flowing" and 1 or nil),
+        },
+    })
 
--- Sewage
-
-minetest.register_node("moreliquids:sewage_flowing", {
-	description = "Sewage (flowing)",
-	inventory_image = minetest.inventorycube("sewage.png"),
-	drawtype = "flowingliquid",
-	tile_images = {"sewage.png"},
-	paramtype = "light",
-	walkable = false,
-	pointable = false,
-	diggable = false,
-	buildable_to = true,
-	liquidtype = "flowing",
-	liquid_alternative_flowing = "moreliquids:sewage_flowing",
-	liquid_alternative_source = "moreliquids:sewage_source",
-	liquid_viscosity = SEWAGE_VISC,
-	post_effect_color = {a=SEWAGE_ALPHA, r=172, g=97, b=0},
-	special_materials = {
-		{image="sewage.png", backface_culling=false},
-		{image="sewage.png", backface_culling=true},
-	},
-})
-
-minetest.register_node("moreliquids:sewage_source", {
-	description = "Sewage",
-	inventory_image = minetest.inventorycube("sewage.png"),
-	drawtype = "liquid",
-	tile_images = {"sewage.png"},
-	paramtype = "light",
-	walkable = false,
-	pointable = false,
-	diggable = false,
-	buildable_to = true,
-	liquidtype = "source",
-	liquid_alternative_flowing = "moreliquids:sewage_flowing",
-	liquid_alternative_source = "moreliquids:sewage_source",
-	liquid_viscosity = SEWAGE_VISC,
-	post_effect_color = {a=SEWAGE_ALPHA, r=172, g=97, b=0},
-	special_materials = {
-		{image="sewage.png", backface_culling=false},
-	},
-})
-
--- Toxic Waste
-
-minetest.register_node("moreliquids:toxic_waste_flowing", {
-	description = "Toxic Waste (flowing)",
-	inventory_image = minetest.inventorycube("toxic_waste.png"),
-	drawtype = "flowingliquid",
-	tile_images = {"toxic_waste.png"},
-	paramtype = "light",
-	walkable = false,
-	pointable = false,
-	diggable = false,
-	buildable_to = true,
-  light_source = default.LIGHT_MAX - 1,
-	liquidtype = "flowing",
-	liquid_alternative_flowing = "moreliquids:toxic_waste_flowing",
-	liquid_alternative_source = "moreliquids:toxic_waste_source",
-	liquid_viscosity = TOXIC_WASTE_VISC,
-	post_effect_color = {a=TOXIC_WASTE_ALPHA, r=41, g=250, b=0},
-	special_materials = {
-		{image="toxic_waste.png", backface_culling=false},
-		{image="toxic_waste.png", backface_culling=true},
-	},
-})
-
-minetest.register_node("moreliquids:toxic_waste_source", {
-	description = "Toxic Waste",
-	inventory_image = minetest.inventorycube("toxic_waste.png"),
-	drawtype = "liquid",
-	tile_images = {"toxic_waste.png"},
-	paramtype = "light",
-	walkable = false,
-	pointable = false,
-	diggable = false,
-	buildable_to = true,
-  light_source = default.LIGHT_MAX - 1,
-	liquidtype = "source",
-	liquid_alternative_flowing = "moreliquids:toxic_waste_flowing",
-	liquid_alternative_source = "moreliquids:toxic_waste_source",
-	liquid_viscosity = TOXIC_WASTE_VISC,
-	post_effect_color = {a=TOXIC_WASTE_ALPHA, r=41, g=250, b=0},
-	special_materials = {
-		{image="toxic_waste.png", backface_culling=false},
-	},
-})
-
--- Cryogel
-
-minetest.register_node("moreliquids:cryogel_flowing", {
-	description = "Cryogel (flowing)",
-	inventory_image = minetest.inventorycube("cryogel.png"),
-	drawtype = "flowingliquid",
-	tile_images = {"cryogel.png"},
-	paramtype = "light",
-	walkable = true,
-	pointable = false,
-	diggable = false,
-	buildable_to = true,
-	liquidtype = "flowing",
-  light_source = default.LIGHT_MAX - 1,
-	liquid_alternative_flowing = "moreliquids:cryogel_flowing",
-	liquid_alternative_source = "moreliquids:cryogel_source",
-	liquid_viscosity = CRYOGEL_VISC,
-	post_effect_color = {a=CRYOGEL_ALPHA, r=100, g=203, b=203},
-	special_materials = {
-		{image="cryogel.png", backface_culling=false},
-		{image="cryogel.png", backface_culling=true},
-	},
-})
-
-minetest.register_node("moreliquids:cryogel_source", {
-	description = "Cryogel",
-	inventory_image = minetest.inventorycube("cryogel.png"),
-	drawtype = "liquid",
-	tile_images = {"cryogel.png"},
-	paramtype = "light",
-	walkable = true,
-	pointable = false,
-	diggable = false,
-	buildable_to = true,
-	liquidtype = "source",
-  light_source = default.LIGHT_MAX - 1,
-	liquid_alternative_flowing = "moreliquids:cryogel_flowing",
-	liquid_alternative_source = "moreliquids:cryogel_source",
-	liquid_viscosity = CRYOGEL_VISC,
-	post_effect_color = {a=CRYOGEL_ALPHA, r=100, g=203, b=203},
-	special_materials = {
-		{image="cryogel.png", backface_culling=false},
-	},
-})
+end
